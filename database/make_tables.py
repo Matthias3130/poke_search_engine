b104b98c1b70ceaf1f93ec723022f40a65d49e69
BlueySilone <simonmc997@gmail.com>
2024-11-17 22:51:50 -0700

    extracted info from api to txt

---
+++ database/make_tables.py
@@ -0,0 +1,70 @@
+# A simple script that extract the wanted information about a pokemon through pokeapi.co
+
+import requests
+from pprint import pprint
+
+def extract_info(data):
+    main_t = "Null"
+    sub_t = "Null"
+    first_ability = "Null"
+    second_ability = "Null"
+    hidden_ability = "Null"
+    
+    for i, typ in enumerate(data["types"]):
+        if main_t == "Null":
+            main_t = data["types"][i]["type"]["name"]
+        else:
+            sub_t = data["types"][i]["type"]["name"]
+    
+    for i, ability in enumerate(data["abilities"]):
+        if data["abilities"][i]["is_hidden"] == True:
+            hidden_ability = data["abilities"][i]["ability"]["name"]
+        else:
+            if first_ability == "Null":
+                first_ability = data["abilities"][i]["ability"]["name"]
+            else:
+                second_ability = data["abilities"][i]["ability"]["name"]
+            
+    poke_stats = {
+        "id": data["id"],
+        "name": data["name"],
+        "img": data["sprites"]["other"]["official-artwork"]["front_default"],
+        "main_t": main_t,
+        "sub_t": sub_t,
+        "first_ability":first_ability,
+        "second_ability": second_ability,
+        "hidden_ability": hidden_ability,
+        "hp": data["stats"][0]["base_stat"],
+        "atk": data["stats"][1]["base_stat"],
+        "def": data["stats"][2]["base_stat"],
+        "sp_atk": data["stats"][3]["base_stat"],
+        "sp_def": data["stats"][4]["base_stat"],
+        "spd": data["stats"][5]["base_stat"]
+    }
+    return poke_stats
+    
+    
+def main():
+    LAST_POKE = 1025
+    f = open("poke_database.txt", "w")
+    
+    for i in range(1,LAST_POKE):
+        progress = (i / LAST_POKE) * 100
+        print(f"Progress: {progress:.2f}%")
+    
+        url = "https://pokeapi.co/api/v2/pokemon/" + str(i)
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            data = response.json()
+            poke_stats = extract_info(data)
+            result = ", ".join(f"{value}" for value in poke_stats.values())
+            # pprint(f"{result}")
+            f.write(result + "\n")
+        else:
+            print("Failed to fetch data:", response.status_code)
+            
+    f.close()
+    
+if __name__ == "__main__":
+    main()
---
+++ database/schema.sql
@@ -0,0 +1,20 @@
+
+
+DROP TABLE IF EXISTS basic_info;
+
+CREATE TABLE basic_info(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    img TEXT NOT NULL,
+    main_t TEXT NOT NULL,
+    sub_t TEXT,
+    first_ability TEXT NOT NULL,
+    second_ability TEXT,
+    hidden_ability TEXT NOT NULL,
+    hp INTEGER NOT NULL,
+    atk INTEGER NOT NULL,
+    def INTEGER NOT NULL,
+    sp_atk INTEGER NOT NULL,
+    sp_def INTEGER NOT NULL,
+    spd INTEGER NOT NULL
+);
